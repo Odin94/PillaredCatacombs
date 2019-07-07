43fca130a58f7c64bfafc8e5a6466b2545be9913
Odin <kammerloher.andreas@gmail.com>
2019-07-07 20:03:03 +0200

    Add view

--- src/main/kotlin/de/odin_matthias/pillaredcatacombs/main.kt
+++ src/main/kotlin/de/odin_matthias/pillaredcatacombs/main.kt
@@ -1,22 +1,13 @@
 package de.odin_matthias.pillaredcatacombs
 
-import org.hexworks.zircon.api.ColorThemes
-import org.hexworks.zircon.api.Components
-import org.hexworks.zircon.api.Screens
+import de.odin_matthias.pillaredcatacombs.view.StartView
 import org.hexworks.zircon.api.SwingApplications
-import org.hexworks.zircon.api.component.ComponentAlignment
 
 @Suppress("ConstantConditionIf")
 fun main(args: Array<String>) {
 
-    val grid = SwingApplications.startTileGrid()
-    val screen = Screens.createScreenFor(grid)
+    val application = SwingApplications.startApplication()
 
-    screen.addComponent(Components.header()
-            .withText("Hello, from Pillared Catacombs!")
-            .withAlignmentWithin(screen, ComponentAlignment.CENTER))
-
-    screen.applyColorTheme(ColorThemes.arc())
-    screen.display()
+    application.dock(StartView())
 
 }
